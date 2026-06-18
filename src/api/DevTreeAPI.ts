b36f129388874a9bf80203990fd833f4120e8eb3
Angel David <angeldoliv85@gmail.com>
2026-06-18 14:09:38 -0600

    agregando componente DevTree y exportando lo

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -1,11 +1,12 @@
 import { isAxiosError } from "axios";
 import api from "../config/axios";
+import type { User } from "../types";
 // aqui vamos a hacer la funcion para comunicarnos con el servidor
 
 export async function getUser() {
     try {
         // el interceptor siempre mandara el dato en la consulta de la api
-        const {data} = await api.get('/user');
+        const {data} = await api<User>('/user');
         return data;
 
     } catch (error) {
